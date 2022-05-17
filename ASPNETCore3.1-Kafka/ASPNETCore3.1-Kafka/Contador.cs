a0e52becb83ac2c62c6b597054274e5d2a1d0cad
MateusMacedo <mateusouza2014@live.com>
2022-05-17 00:29:26 -0300

    feat: Adicionando Kafka Topico

---
+++ ASPNETCore3.1-Kafka/ASPNETCore3.1-Kafka/Contador.cs
@@ -0,0 +1,14 @@
+﻿namespace ASPNETCore3._1_Kafka
+{
+    public class Contador
+    {
+        private int _valorAtual = 0;
+
+        public int ValorAtual { get => _valorAtual; }
+
+        public void Incrementar()
+        {
+            _valorAtual++;
+        }
+    }
+}
---
+++ ASPNETCore3.1-Kafka/Domain/Entidades/Acao.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain
+{
+    public class Acao
+    {
+        public string Codigo { get; set; }
+        public double? Valor { get; set; }
+    }
+}
